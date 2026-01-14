8e1a3421d3e2d494335b071a30968b03cb2dff66
Talha <ts0306706@gmail.com>
2026-01-14 14:50:44 +0500

    checking

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -7,10 +7,14 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
 
+console.log("------------------------------------------------");
+console.log("DEBUG: ACTIVE API KEY ->", process.env.SHOPIFY_API_KEY);
+console.log("DEBUG: DATABASE URL   ->", process.env.DATABASE_URL);
+console.log("------------------------------------------------");
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.October25,
+  apiVersion: ApiVersion.January26,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
