801da16d9bd95ceae49995c29d357052814a8da7
socobaapps-burhan <socoba.apps@gmail.com>
2026-02-14 01:15:55 +0500

    ok

--- prisma/migrations/20260213114953_add_custom_id_counter_and_modify_orderblock_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "AppSetting" (
-    "key" TEXT NOT NULL PRIMARY KEY,
-    "value" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "AppSetting_key_key" ON "AppSetting"("key");
